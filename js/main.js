521fb17c3d3b63de89882e99e0be769a7a24333d
xeontem <xeontem@gmail.com>
2017-02-15 22:02:32 +0300

    position_and_float/add: implement all files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+	var page1 = document.getElementById('page1'); 
+	var page2 = document.getElementById('page2'); 
+	var page3 = document.getElementById('page3');
+	page2.style.height = page1.scrollHeight - 43 + 'px';
+	page2.style.top = -page1.scrollHeight + 10 + 'px';
+	page3.style.height = page1.scrollHeight - 43 + 'px';
+	page3.style.top = -page1.scrollHeight -page1.scrollHeight + 26 + 'px';;
+}
