3fd6eb954cee6cffc0e474a6e2e8c53dd7e279da
xeontem <xeontem@gmail.com>
2017-02-21 17:29:24 +0300

    pos_nd_floats/fix: remove all js from page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+/*window.onload = function(){
 	var page1 = document.getElementById('page1'); 
 	var page2 = document.getElementById('page2'); 
 	var page3 = document.getElementById('page3');
@@ -6,4 +6,4 @@ window.onload = function(){
 	page2.style.top = -page1.scrollHeight + 10 + 'px';
 	page3.style.height = page1.scrollHeight - 43 + 'px';
 	page3.style.top = -page1.scrollHeight -page1.scrollHeight + 26 + 'px';;
-}
+}*/
